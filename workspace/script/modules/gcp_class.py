811b7497d58d044d0d1064c9f800e718cd5e369f
hopopyuya <hopopyuya@YuyanoMacBook-Air.local>
2024-10-20 23:30:17 +0900

    modified script

--- workspace/script/modules/gcp_class.py
+++ workspace/script/modules/gcp_class.py
@@ -24,6 +24,24 @@ class Gcs_client:
         blob.upload_from_filename(local_file_path)
         print(f"Uploaded {local_file_path} to gs://{bucket_name}/{destination_blob_name}")
 
+    def load_data_to_bigquery(self, source_uri):
+        dataset_ref = self.bq_client.dataset(self.dataset_name)
+        table_ref = dataset_ref.table(self.table_name)
+
+        job_config = bigquery.LoadJobConfig(
+            source_format=bigquery.SourceFormat.PARQUET,
+            write_disposition=bigquery.WriteDisposition.WRITE_APPEND
+        )
+        
+        load_job = self.bq_client.load_table_from_uri(
+            source_uri, table_ref, job_config=job_config
+        )
+
+        # ロードジョブが完了するまで待つ
+        load_job.result()
+
+        tqdm.write(f"Data loaded into BigQuery table {self.dataset_name}.{self.table_name} from {source_uri}.")ss
+
 class Bigquery_client:
     def __init__(self):
         self.client = bigquery.Client()
